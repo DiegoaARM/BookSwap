742c05e359aa084836c3f6824c5c7b44679412cc
DiegoaARM <juan.duque31@udea.edu.co>
2024-11-16 15:22:59 -0500

    endpoint de copy_image creado

--- src/main/java/BookSwap/controller/Copy_imageController.java
+++ src/main/java/BookSwap/controller/Copy_imageController.java
@@ -15,27 +15,27 @@ public class Copy_imageController {
     @Autowired
     private ICopy_image copy_imageService;
 
-    @PostMapping(path = "copy")
+    @PostMapping(path = "copy_image")
     public Copy_image create(@RequestBody Copy_image copy_image) {
         return copy_imageService.save(copy_image);
     }
 
-    @PutMapping(path = "copy")
+    @PutMapping(path = "copy_image")
     public Copy_image update(@RequestBody Copy_image copy_image) {
         return copy_imageService.save(copy_image);
     }
 
-    @DeleteMapping(path = "copy/{id}")
+    @DeleteMapping(path = "copy_image/{id}")
     public void delete(@PathVariable Integer id) {
         copy_imageService.delete(copy_imageService.findById(id));
     }
 
-    @GetMapping(path = "copy/{id}")
+    @GetMapping(path = "copy_image/{id}")
     public Copy_image findById(@PathVariable Integer id) {
         return copy_imageService.findById(id);
     }
 
-    @GetMapping(path = "copies")
+    @GetMapping(path = "copy_images")
     public List<Copy_image> findAll(){
         return copy_imageService.findAll();
     }
